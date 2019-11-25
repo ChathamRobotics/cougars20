4ff4ddadda6f65598ec376ff3c1ca6c179f1f9a4
Timothy McEvoy <48994156+TimothyMcEvoy@users.noreply.github.com>
2019-11-25 08:29:21 -0500

    Slowed down four bar servos so that it works better and changed some other small things.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMechnum.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpMechnum.java
@@ -2,10 +2,10 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-//import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.hardware.Servo;
-
 import java.lang.*;
 
 //11248 //Cougars Property >:D
@@ -191,4 +191,3 @@ public class TeleOpMechnum extends LinearOpMode {
 
         }
     }}
-
