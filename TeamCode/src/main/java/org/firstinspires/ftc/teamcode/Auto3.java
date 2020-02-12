18748c56ae10ed2624d41cc2f6acb53f806bc34b
Timothy McEvoy <48994156+TimothyMcEvoy@users.noreply.github.com>
2020-02-12 10:44:37 -0500

    Did work on auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto3.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto3.java
@@ -1,30 +1,20 @@
 package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
-
 @Autonomous(name = "Auto3", group = "LinearOpMode")
-
 public class Auto3 extends LinearOpMode {
-
     private ElapsedTime runtime = new ElapsedTime();
     private DcMotor leftFront, rightFront, leftRear, rightRear, intakeMotorLeft, intakeMotorRight, liftmotorLeft, liftmotorRight;
     private Servo iHold, fGrabLeft, fGrabRight, claw, fBarLeft, fBarRight;
     //right trigger up
     //left trigger down
-
-
-
     @Override
     public void runOpMode() {
-
-
         telemetry.addData("init","init");
         telemetry.update();
-
         //wheel motors hardware mapping
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
@@ -34,19 +24,16 @@ public class Auto3 extends LinearOpMode {
         rightRear = hardwareMap.get(DcMotor.class, "rightRear");
         leftRear.setDirection(DcMotor.Direction.FORWARD);
         rightRear.setDirection(DcMotor.Direction.REVERSE);
-
         //Intake hardware mapping
         intakeMotorLeft = hardwareMap.get(DcMotor.class, "intakeMotorLeft");
         intakeMotorRight = hardwareMap.get(DcMotor.class, "intakeMotorRight");
         intakeMotorLeft.setDirection(DcMotor.Direction.FORWARD);
         intakeMotorRight.setDirection(DcMotor.Direction.REVERSE);
-
         //Lift hardware mapping
         liftmotorLeft = hardwareMap.get(DcMotor.class, "liftmotorLeft");
         liftmotorRight = hardwareMap.get(DcMotor.class, "liftmotorRight");
         liftmotorLeft.setDirection(DcMotor.Direction.REVERSE);
         liftmotorRight.setDirection(DcMotor.Direction.FORWARD);
-
         //Servo hardware mapping
         iHold = hardwareMap.get(Servo.class, "iHold");
         fGrabLeft = hardwareMap.get(Servo.class, "fGrabLeft");
@@ -54,18 +41,9 @@ public class Auto3 extends LinearOpMode {
         claw = hardwareMap.get(Servo.class, "claw");
         fBarLeft = hardwareMap.get(Servo.class, "fBarLeft");
         fBarRight = hardwareMap.get(Servo.class, "fBarRight");
-
-
-
         waitForStart();
-
-
-
-
         //This has the robot move up and then grab the plate
-
         ElapsedTime runtime = new ElapsedTime();
-
         //move forward for .25 sec and lift grabbers
         while (opModeIsActive() && (runtime.seconds() <= 0.25 ))
         {
@@ -77,68 +55,76 @@ public class Auto3 extends LinearOpMode {
             rightRear.setPower(.5);
         }
 
-        //turn right for .25 sec
-        while (opModeIsActive()&&(runtime.seconds() <= 0.8 ))
-        {
-            leftFront.setPower(.5);
-            rightFront.setPower(-.5);
-            leftRear.setPower(.5);
-            rightRear.setPower(-.5);
-        }
-
-        //.7
-        //move forward for .25 sec
-        while (opModeIsActive() && (runtime.seconds() <= 1.65 ))
-        {
-
-            leftFront.setPower(.5);
-            rightFront.setPower(.5);
-            leftRear.setPower(.5);
-            rightRear.setPower(.5);
-        }
-
         //turn left for .25 sec
-        while (opModeIsActive() && (runtime.seconds() <= 2.2))
-        {
 
-            leftFront.setPower(-.5);
-            rightFront.setPower(.5);
-            leftRear.setPower(-.5);
-            rightRear.setPower(.5);
-        }
+            //turn right for .25 sec
+            while (opModeIsActive()&&(runtime.seconds() <= 0.6 ))
+            {
+                leftFront.setPower(.5);
+                rightFront.setPower(-.5);
+                leftRear.setPower(.5);
+                rightRear.setPower(-.5);
+            }
+
+        //move forward for .25 sec
+
+            while (opModeIsActive() && (runtime.seconds() <= 1.25 ))
+            {
+
+                leftFront.setPower(.5);
+                rightFront.setPower(.5);
+                leftRear.setPower(.5);
+                rightRear.setPower(.5);
+            }
+
+        //turn right for .25 sec
+
+            //turn left for .25 sec
+            while (opModeIsActive() && (runtime.seconds() <= 1.5))
+            {
+
+                leftFront.setPower(-.5);
+                rightFront.setPower(.5);
+                leftRear.setPower(-.5);
+                rightRear.setPower(.5);
+            }
 
         //move forward to build plate for .8 sec
-        while (opModeIsActive() &&(runtime.seconds() <= 4.2 ))
-        {
 
-            leftFront.setPower(.5);
-            rightFront.setPower(.5);
-            leftRear.setPower(.5);
-            rightRear.setPower(.5);
-        }
+            while (opModeIsActive() &&(runtime.seconds() <= 2.3 ))
+            {
 
-        //grab build plate for .8 sec
-        while (opModeIsActive() && (runtime.seconds() <= 5))
-        {
-            leftFront.setPower(0);
-            rightFront.setPower(0);
-            leftRear.setPower(0);
-            rightRear.setPower(0);
-            sleep(100);
-            fGrabLeft.setPosition(90);
-            fGrabRight.setPosition(0);
+                leftFront.setPower(.5);
+                rightFront.setPower(.5);
+                leftRear.setPower(.5);
+                rightRear.setPower(.5);
+            }
 
-        }
-//1.7
+        //grab build plate for .7 sec
+
+            //grab build plate for .8 sec
+            while (opModeIsActive() && (runtime.seconds() <= 2.8))
+            {
+                leftFront.setPower(0);
+                rightFront.setPower(0);
+                leftRear.setPower(0);
+                rightRear.setPower(0);
+                sleep(100);
+                fGrabLeft.setPosition(90);
+                fGrabRight.setPosition(0);
+
+            }
+
         //This has the robot move the plate into the appropriate zone - moves backwards for 2.8 sec
-        while (opModeIsActive() && (runtime.seconds() <= 6.2))
-        {
-            leftFront.setPower(-.5);
-            rightFront.setPower(-.5);
-            leftRear.setPower(-.5);
-            rightRear.setPower(-.5);
-        }
-        while (opModeIsActive() && (runtime.seconds() <= 7.5))
+
+            while (opModeIsActive() && (runtime.seconds() <= 4.5))
+            {
+                leftFront.setPower(-.5);
+                rightFront.setPower(-.5);
+                leftRear.setPower(-.5);
+                rightRear.setPower(-.5);
+            }
+        while (opModeIsActive() && (runtime.seconds() <= 5.8))
         {
             leftFront.setPower(.5);
             rightFront.setPower(-.5);
@@ -147,18 +133,18 @@ public class Auto3 extends LinearOpMode {
         }
 
         //lift up grabbers for .7 sec
-        while (opModeIsActive() &&  (runtime.seconds() <= 8.1)) {
+        while (opModeIsActive() &&  (runtime.seconds() <= 6)) {
             fGrabLeft.setPosition(0);
             fGrabRight.setPosition(90);
         }
-        while (opModeIsActive() && (runtime.seconds() <= 8.1))
+        while (opModeIsActive() && (runtime.seconds() <= 6.4))
         {
             leftFront.setPower(.5);
             rightFront.setPower(-.5);
             leftRear.setPower(.5);
             rightRear.setPower(-.5);
         }
-        while (opModeIsActive() && (runtime.seconds() <= 9.4)) {
+        while (opModeIsActive() && (runtime.seconds() <= 7.7)) {
             leftFront.setPower(-.5);
             rightFront.setPower(-.5);
             leftRear.setPower(-.5);
@@ -174,20 +160,5 @@ public class Auto3 extends LinearOpMode {
                 rightRear.setPower(1);
             }
 */
-
-
-
-
-
-
-
-
-
-
-
-
     }
 }
-
-
-
