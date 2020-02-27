58720461588f11bd95fc1e883c149c4724168d04
Timothy McEvoy <48994156+TimothyMcEvoy@users.noreply.github.com>
2020-02-27 09:33:34 -0500

    Worked on Vuforia, the robot is now scared of the skystone so when it sees one it runs away. Next time it hopefully wont be scared.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoV.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoV.java
@@ -13,6 +13,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefau
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 import java.util.ArrayList;
 import java.util.List;
+import com.qualcomm.robotcore.hardware.*;
 import static org.firstinspires.ftc.robotcore.external.navigation.AngleUnit.DEGREES;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.XYZ;
 import static org.firstinspires.ftc.robotcore.external.navigation.AxesOrder.YZX;
@@ -23,6 +24,10 @@ import android.graphics.Bitmap;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorController;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.RobotLog;
 import com.qualcomm.robotcore.util.ThreadPool;
 import com.vuforia.Frame;
@@ -50,6 +55,7 @@ import java.util.Locale;
 
 
 
+
 @Autonomous(name="AutoV", group ="Concept")
 public class AutoV extends LinearOpMode {
     WebcamName webcamName;
@@ -60,29 +66,18 @@ public class AutoV extends LinearOpMode {
 
     private static final String VUFORIA_KEY =
             "AaHZ4HX/////AAABmUKBS4vEjk9Fklm446e72CEUymsNs4n0biEwDRp9JLz8IgtE2EbmyEkbvBRbIbKpGOZzksOpIMsyCxjkM/rXcyML+HagK0BS3rm/G1q0mHa7fR4cVrv+6cThrohhh7tOCglAxVaUnV/GaLIrghdTdam+QazAsRudGYNsS7/KctcjPg7A2CCsLpKw9B1PVvAwaLGOTbSdDetdDTCvbydmJEqJwaXzNKRH01i+RYc3PbtLmlYKym9upNH/Bgumo03oPBJz0hZM59B1tr1DZqfZEwOUSfSljoJ3jEq875srZ1VnVUX8KP1ZoIjn8wPS4gN6JHaDUEhprDMm0hINm9c2CTsUjUGNohdk+ZtnJlJ6fMLY";
-
-
+    private DcMotor leftFront, rightFront, leftRear, rightRear, intakeMotorLeft, intakeMotorRight, liftmotorLeft, liftmotorRight;
+    private Servo iHold, fGrabLeft, fGrabRight, claw, fBarLeft, fBarRight;
     private static final float mmPerInch        = 25.4f;
     private static final float mmTargetHeight   = (6) * mmPerInch;
-
     private static final float stoneZ = 2.00f * mmPerInch;
-
-
     private static final float bridgeZ = 6.42f * mmPerInch;
     private static final float bridgeY = 23 * mmPerInch;
     private static final float bridgeX = 5.18f * mmPerInch;
     private static final float bridgeRotY = 59;
     private static final float bridgeRotZ = 180;
-
-
-
-
-
-
     private static final float halfField = 72 * mmPerInch;
     private static final float quadField  = 36 * mmPerInch;
-
-
     private OpenGLMatrix lastLocation = null;
     private VuforiaLocalizer vuforia = null;
     private boolean targetVisible = false;
@@ -90,11 +85,53 @@ public class AutoV extends LinearOpMode {
     private float phoneYRotate    = 0;
     private float phoneZRotate    = 0;
 
-    @Override public void runOpMode() {
 
+
+    @Override public void runOpMode() throws InterruptedException
+    {
+        telemetry.addData("init","init");
+        telemetry.update();
+
+        //wheel motors hardware mapping
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.REVERSE);
+        leftRear = hardwareMap.get(DcMotor.class, "leftRear");
+        rightRear = hardwareMap.get(DcMotor.class, "rightRear");
+        leftRear.setDirection(DcMotor.Direction.FORWARD);
+        rightRear.setDirection(DcMotor.Direction.REVERSE);
+
+        //Intake hardware mapping
+        intakeMotorLeft = hardwareMap.get(DcMotor.class, "intakeMotorLeft");
+        intakeMotorRight = hardwareMap.get(DcMotor.class, "intakeMotorRight");
+        intakeMotorLeft.setDirection(DcMotor.Direction.FORWARD);
+        intakeMotorRight.setDirection(DcMotor.Direction.REVERSE);
+
+        //Lift hardware mapping
+        liftmotorLeft = hardwareMap.get(DcMotor.class, "liftmotorLeft");
+        liftmotorRight = hardwareMap.get(DcMotor.class, "liftmotorRight");
+        liftmotorLeft.setDirection(DcMotor.Direction.REVERSE);
+        liftmotorRight.setDirection(DcMotor.Direction.FORWARD);
+
+        //Servo hardware mapping
+        iHold = hardwareMap.get(Servo.class, "iHold");
+        fGrabLeft = hardwareMap.get(Servo.class, "fGrabLeft");
+        fGrabRight = hardwareMap.get(Servo.class, "fGrabRight");
+        claw = hardwareMap.get(Servo.class, "claw");
+        fBarLeft = hardwareMap.get(Servo.class, "fBarLeft");
+        fBarRight = hardwareMap.get(Servo.class, "fBarRight");
+
+        //Camera hardware mapping
         webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
         int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+
+
+       /* leftFront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        rightFront.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        leftRear.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS);
+        rightRear.setMode(DcMotorController.RunMode.RUN_USING_ENCODERS); */
 
 
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
@@ -102,6 +139,7 @@ public class AutoV extends LinearOpMode {
 
 
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
+        ElapsedTime runtime = new ElapsedTime();
 
 
         VuforiaTrackables targetsSkyStone = this.vuforia.loadTrackablesFromAsset("Skystone");
@@ -254,8 +292,21 @@ public class AutoV extends LinearOpMode {
                 double xPosition = translation.get(0);
                 if(xPosition < -10){
                     positionSkystone = "left";
+                    fGrabLeft.setPosition(0);
+                    fGrabRight.setPosition(90);
+                    leftFront.setPower(.5);
+                    rightFront.setPower(.5);
+                    leftRear.setPower(.5);
+                    rightRear.setPower(.5);
                 }else{
                     positionSkystone = "center";
+                    fGrabLeft.setPosition(0);
+                    fGrabRight.setPosition(90);
+                    leftFront.setPower(.5);
+                    rightFront.setPower(.5);
+                    leftRear.setPower(.5);
+                    rightRear.setPower(.5);
+
                 }
 
 
@@ -268,10 +319,17 @@ public class AutoV extends LinearOpMode {
             }
             telemetry.addData("Skystone Position", positionSkystone);
             telemetry.update();
+
+
+
+
+
         }
 
 
 
-        targetsSkyStone.deactivate();
+
+
+     targetsSkyStone.deactivate();
     }
 }
